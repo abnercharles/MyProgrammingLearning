4a1665ee9c45f5675766170a0e5cce961a2736b2
abnercharles <abnercharles@sjtu.edu.cn>
2016-04-15 16:23:43 +0800

    feel alright

---
+++ vector2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> vec(9,4);
+    cout << vec [2];
+   /* vec.push_back(3);
+    vector<int>::iterator it;
+    for (it = vec.begin();it != vec.end; ++it;)
+        cout << *it << endl;*/
+}
